9349f1dec9030498972c9600b9cc679494c7ec0c
aerain <aerain@localhost.localdomain>
2017-09-10 02:30:44 +0900

    20170910

--- app.js
+++ app.js
@@ -49,6 +49,6 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-app.listen(80);
+// app.listen();
 
 module.exports = app;
--- public/javascripts/haksa.js
+++ public/javascripts/haksa.js
@@ -1,15 +1,25 @@
 module.exports = function ($) {
-
   strjson += '{ "title" : "제주대학교 학사 일정" ,';
-  haksatable = $('.sub_content_list > dd > dl > dd > table');
-  haksatable.find('tbody > tr > td').each(function(i) {
-    temp = $(this).text().replace(/(\s*~\s*)/gi, ' ~ ');
-    temp = temp.replace(/(^\s*)|(\s*$)/gi, '');
-    strjson += '"haksa' + i + '" : "' + temp + '", ';
+
+  haksatable = $('.table.border_left.border_top_blue.font09 > tbody');
+
+  haksatable.each(function(i) {
+    strjson += ' "month' + (i + 1)+ '" : {';
+
+    $(this).find('tr > td').each(function (j) {
+      temp = $(this).text().replace(/(\s*~\s*)/gi, ' ~ ');
+      temp = temp.replace(/(^\s*)|(\s*$)/gi, '');
+      strjson += '"haksa' + (j + 1) + '" : "' + temp + '", ';
+    })
+    strjson += '"bla" : ""}, '
   });
+  // haksatable.find('tbody > tr > td').each(function(i) {
+  //   temp = $(this).text().replace(/(\s*~\s*)/gi, ' ~ ');
+  //   temp = temp.replace(/(^\s*)|(\s*$)/gi, '');
+  //   strjson += '"haksa' + i + '" : "' + temp + '", ';
+  // });
   strjson += '"blank" : ""}';
   console.log(strjson);
-
 
   return JSON.parse(strjson);
 }
