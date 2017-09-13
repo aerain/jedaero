4c0df5f84dcea999afe757750bff16ffdcdbc419
wkdlfhtm1 <wkdlfhtm1@naver.com>
2017-09-13 09:40:51 +0900

    First name

--- routes/libserch.js
+++ routes/libserch.js
@@ -1,23 +1,31 @@
 var request = require('request');
 var cheerio = require('cheerio');
 
-var url = "http://lib.jejunu.ac.kr/#/search/si?all=1%7Ck%7Ca%7C%ED%83%95%EC%88%98%EC%9C%A1";
-
+var url = "http://lib.jejunu.ac.kr/pyxis-api/1/collections/1/search?all=k%7Ca%7C%E3%84%B1&abc="
+            +"http://lib.jejunu.ac.kr/pyxis-api/1/collections/1/search?all=k%7Ca%7C%E3%84%B4&abc="
+            +"http://lib.jejunu.ac.kr/pyxis-api/1/collections/1/search?all=k%7Ca%7C%E3%84%B7&abc="
+            +"http://lib.jejunu.ac.kr/pyxis-api/1/collections/1/search?all=k%7Ca%7C%E3%84%B9&abc="
+            +"http://lib.jejunu.ac.kr/pyxis-api/1/collections/1/search?all=k%7Ca%7C%E3%85%81&abc="
+            +"http://lib.jejunu.ac.kr/pyxis-api/1/collections/1/search?all=k%7Ca%7C%E3%85%82&abc="
+            +"http://lib.jejunu.ac.kr/pyxis-api/1/collections/1/search?all=k%7Ca%7C%E3%85%85&abc="
+            +"http://lib.jejunu.ac.kr/pyxis-api/1/collections/1/search?all=k%7Ca%7C%E3%85%87&abc="
+            +"http://lib.jejunu.ac.kr/pyxis-api/1/collections/1/search?all=k%7Ca%7C%E3%85%88&abc="
+            +"http://lib.jejunu.ac.kr/pyxis-api/1/collections/1/search?all=k%7Ca%7C%E3%85%8A&abc="
+            +"http://lib.jejunu.ac.kr/pyxis-api/1/collections/1/search?all=k%7Ca%7C%E3%85%8B&abc="
+            +"http://lib.jejunu.ac.kr/pyxis-api/1/collections/1/search?all=k%7Ca%7C%E3%85%8C&abc="
+            +"http://lib.jejunu.ac.kr/pyxis-api/1/collections/1/search?all=k%7Ca%7C%E3%85%8C&abc="
+            +"http://lib.jejunu.ac.kr/pyxis-api/1/collections/1/search?all=k%7Ca%7C%E3%85%8E&abc="
+            +"http://lib.jejunu.ac.kr/pyxis-api/1/collections/1/search?all=k%7Ca%7C%E3%84%B2&abc="
+            +"http://lib.jejunu.ac.kr/pyxis-api/1/collections/1/search?all=k%7Ca%7C%E3%84%B8&abc="
+            +"http://lib.jejunu.ac.kr/pyxis-api/1/collections/1/search?all=k%7Ca%7C%E3%85%86&abc=";
 request(url,function (err, res, html) {
+    if (!err) {
+        var $ = cheerio.load(html);
+        console.log(html);
+        // date = $('titleStatment').each(function () {
+        // console.log($(this).text());
 
-    if(!err){
-        var $ = cheerio.load(html,{normalizeWhitespace : false, xmlMode : false, decodeEntities : true});
-
-
-        $('script').each(function () {
-            console.log('JS:%s',$(this).text());
-
-
-        });
-   }
-    else{
-        console.log('ERR: %j\t%j’,err,res.statusCode');
+        //});
 
     }
-
-});
+})
