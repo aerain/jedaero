f2427613470c8565ac46d38be15f4819bafc0f09
wkdlfhtm1 <wkdlfhtm1@naver.com>
2017-09-16 22:49:52 +0900

    Frist name

--- routes/libserch.js
+++ routes/libserch.js
@@ -1,10 +1,17 @@
 var request = require('request');
 var cheerio = require('cheerio');
+var iconv = require('iconv-lite');
 
-var url =
+var url = "%EC%86%8C%EB%82%98%EA%B8%B0";
 
-request(url,function (err, res, html) {
+var options = {
+    uri : url,
+    encoding : null
+};
+request(options,function (err, res, html) {
     if (!err) {
+        var strContents = new Buffer(html);
+        html = iconv.decode(strContents, 'utf-8').toString();
         var $ = cheerio.load(html);
         console.log(html);
         // date = $('titleStatment').each(function () {
