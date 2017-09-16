7044833fa2f25fe1f1f7ff56a55aaa75c757126b
wkdlfhtm1 <wkdlfhtm1@naver.com>
2017-09-16 22:38:32 +0900

    First-name

--- routes/libserch.js
+++ routes/libserch.js
@@ -1,23 +1,8 @@
 var request = require('request');
 var cheerio = require('cheerio');
 
-var url = "http://lib.jejunu.ac.kr/pyxis-api/1/collections/1/search?all=k%7Ca%7C%E3%84%B1&abc="
-            +"http://lib.jejunu.ac.kr/pyxis-api/1/collections/1/search?all=k%7Ca%7C%E3%84%B4&abc="
-            +"http://lib.jejunu.ac.kr/pyxis-api/1/collections/1/search?all=k%7Ca%7C%E3%84%B7&abc="
-            +"http://lib.jejunu.ac.kr/pyxis-api/1/collections/1/search?all=k%7Ca%7C%E3%84%B9&abc="
-            +"http://lib.jejunu.ac.kr/pyxis-api/1/collections/1/search?all=k%7Ca%7C%E3%85%81&abc="
-            +"http://lib.jejunu.ac.kr/pyxis-api/1/collections/1/search?all=k%7Ca%7C%E3%85%82&abc="
-            +"http://lib.jejunu.ac.kr/pyxis-api/1/collections/1/search?all=k%7Ca%7C%E3%85%85&abc="
-            +"http://lib.jejunu.ac.kr/pyxis-api/1/collections/1/search?all=k%7Ca%7C%E3%85%87&abc="
-            +"http://lib.jejunu.ac.kr/pyxis-api/1/collections/1/search?all=k%7Ca%7C%E3%85%88&abc="
-            +"http://lib.jejunu.ac.kr/pyxis-api/1/collections/1/search?all=k%7Ca%7C%E3%85%8A&abc="
-            +"http://lib.jejunu.ac.kr/pyxis-api/1/collections/1/search?all=k%7Ca%7C%E3%85%8B&abc="
-            +"http://lib.jejunu.ac.kr/pyxis-api/1/collections/1/search?all=k%7Ca%7C%E3%85%8C&abc="
-            +"http://lib.jejunu.ac.kr/pyxis-api/1/collections/1/search?all=k%7Ca%7C%E3%85%8C&abc="
-            +"http://lib.jejunu.ac.kr/pyxis-api/1/collections/1/search?all=k%7Ca%7C%E3%85%8E&abc="
-            +"http://lib.jejunu.ac.kr/pyxis-api/1/collections/1/search?all=k%7Ca%7C%E3%84%B2&abc="
-            +"http://lib.jejunu.ac.kr/pyxis-api/1/collections/1/search?all=k%7Ca%7C%E3%84%B8&abc="
-            +"http://lib.jejunu.ac.kr/pyxis-api/1/collections/1/search?all=k%7Ca%7C%E3%85%86&abc=";
+var url =
+
 request(url,function (err, res, html) {
     if (!err) {
         var $ = cheerio.load(html);
