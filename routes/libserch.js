6e81d779e7293ddb3fff45e4fda0c8060fdb06dd
wkdlfhtm1 <wkdlfhtm1@naver.com>
2017-09-17 03:06:55 +0900

    Frist name

--- routes/libserch.js
+++ routes/libserch.js
@@ -2,7 +2,7 @@ var request = require('request');
 var cheerio = require('cheerio');
 var iconv = require('iconv-lite');
 
-var url = "%EC%86%8C%EB%82%98%EA%B8%B0";
+var url = "http://lib.jejunu.ac.kr/pyxis-api/1/collections/1/search?all=1|k|a|%EC%86%8C%EB%82%98%EA%B8%B0&facet=false&max=120";
 
 var options = {
     uri : url,
@@ -13,7 +13,11 @@ request(options,function (err, res, html) {
         var strContents = new Buffer(html);
         html = iconv.decode(strContents, 'utf-8').toString();
         var $ = cheerio.load(html);
-        console.log(html);
+
+        dbjson = JSON.parse(html);
+
+        console.log(dbjson.data.list[1].titleStatement);
+        // res.render('info/libserch', dbjson);
         // date = $('titleStatment').each(function () {
         // console.log($(this).text());
 
--- routes/new.js
+++ routes/new.js
@@ -1,2 +1,9 @@
-var request = require('request');
 var iconv = require('iconv-lite');
+
+var asc = "%ED%82%A4%EB%B3%B4%EB%93%9C";
+
+var strContents = new Buffer(asc);
+
+date = iconv.decode(strContents,"utf-8").toString();
+
+console.log(date);
