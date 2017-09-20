ccd207d6ebcb55ff1e1f58ddc906a9c79840c70f
wkdlfhtm1 <wkdlfhtm1@naver.com>
2017-09-20 09:41:47 +0900

    First name

--- routes/libserch.js
+++ routes/libserch.js
@@ -6,22 +6,22 @@ var cheerio = require('cheerio');
 var iconv = require('iconv-lite');
 
 
-var options = {
+//var options = {
     // uri : "http://lib.jejunu.ac.kr/pyxis-api/1/collections/1/search?all=1|k|a|%EC%86%8C%EB%82%98%EA%B8%B0&facet=false&max=120",
-    uri : "http://lib.jejunu.ac.kr/pyxis-api/1/collections/1/search?all=1|k|a|%EC%86%8C%EB%82%98%EA%B8%B0&facet=false&max=65535",
-    encoding : null,
+    url = "http://lib.jejunu.ac.kr/pyxis-api/1/collections/1/search?all=1|k|a|%EC%86%8C%EB%82%98%EA%B8%B0&facet=false&max=65535";
+   // encoding : null,
 
-};
+//};
 
 router.get('/libserch', function(req,res,next){
-    request(options,function (err, ress, html) {
+    request(url,function (err, ress, html) {
     if (!err) {
-        var strContents = new Buffer(html);
-        html = iconv.decode(strContents, 'utf-8').toString();
 
-        dbjson = JSON.parse(html);
+        dbjson = html;
+
         dbjson.title = "영어공부해";
-        console.log(html);
+        console.log(dbjson);
+
         res.render('info/libserch', dbjson);
 
 
