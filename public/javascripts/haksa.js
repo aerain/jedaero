7cdbaa03a4f46963c003eca611d1446441cb657c
aerain <j40f893@gmail.com>
2017-10-09 14:45:49 +0900

    20171009

--- public/javascripts/haksa.js
+++ public/javascripts/haksa.js
@@ -1,25 +1,40 @@
-module.exports = function ($) {
-  strjson += '{ "title" : "제주대학교 학사 일정" ,';
+module.exports = function($) {
+    var haksa = new Object();
 
-  haksatable = $('.table.border_left.border_top_blue.font09 > tbody');
+    haksa.title = "제주대학교 학사 일정";
 
-  haksatable.each(function(i) {
-    strjson += ' "month' + (i + 1)+ '" : {';
+    // 학사일정의 각 월별 학사표.
+    haksatable = $('.table.border_left.border_top_blue.font09 > tbody');
 
-    $(this).find('tr > td').each(function (j) {
-      temp = $(this).text().replace(/(\s*~\s*)/gi, ' ~ ');
-      temp = temp.replace(/(^\s*)|(\s*$)/gi, '');
-      strjson += '"haksa' + (j + 1) + '" : "' + temp + '", ';
-    })
-    strjson += '"bla" : ""}, '
-  });
-  // haksatable.find('tbody > tr > td').each(function(i) {
-  //   temp = $(this).text().replace(/(\s*~\s*)/gi, ' ~ ');
-  //   temp = temp.replace(/(^\s*)|(\s*$)/gi, '');
-  //   strjson += '"haksa' + i + '" : "' + temp + '", ';
-  // });
-  strjson += '"blank" : ""}';
-  console.log(strjson);
+    haksatable.each(function(i) {
+      monthObject = {};
+        $(this).find('tr > td'). each(function(j) {
 
-  return JSON.parse(strjson);
+          // temp는 각각의 화이트 스페이스를 제거해낸 텍스트
+          temp = $(this).text().replace(/(\s*~\s*)/gi, ' ~ ');
+          temp = temp.replace(/(^\s*)|(\s*$)/g, '');
+
+          
+          monthObject["haksa" + (j + 1)] = temp;
+        });
+      console.log(monthObject);
+    });
+
+
+    /*haksatable = $('.table.border_left.border_top_blue.font09 > tbody');
+    haksatable.each(function(i) {
+        strjson += ' "month' + (i + 1) + '" : {';
+
+        $(this).find('tr > td').each(function(j) {
+            temp = $(this).text().replace(/(\s*~\s*)/gi, ' ~ ');
+            temp = temp.replace(/(^\s*)|(\s*$)/gi, '');
+            strjson += '"haksa' + (j + 1) + '" : "' + temp + '", ';
+        })
+        strjson += '"bla" : ""}, '
+    });
+    strjson += '"blank" : ""}';
+    console.log(strjson);*/
+
+    console.log(JSON.stringify(haksa));
+    return haksa;
 }
