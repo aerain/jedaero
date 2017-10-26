50961c2de336c2c066ca3359a7ef6af481f3070b
aerain <j40f893@gmail.com>
2017-10-26 17:54:47 +0900

    20171026haksa

--- public/javascripts/haksa.js
+++ public/javascripts/haksa.js
@@ -2,39 +2,25 @@ module.exports = function($) {
     var haksa = new Object();
 
     haksa.title = "제주대학교 학사 일정";
+    haksa.month = [];
 
     // 학사일정의 각 월별 학사표.
     haksatable = $('.table.border_left.border_top_blue.font09 > tbody');
 
     haksatable.each(function(i) {
-      monthObject = {};
+      monthObject = {
+        schedule : []
+      };
+      monthObject["month_title"] = $(this).find()
         $(this).find('tr > td'). each(function(j) {
-
+          day = {};
           // temp는 각각의 화이트 스페이스를 제거해낸 텍스트
           temp = $(this).text().replace(/(\s*~\s*)/gi, ' ~ ');
           temp = temp.replace(/(^\s*)|(\s*$)/g, '');
-
-          
-          monthObject["haksa" + (j + 1)] = temp;
+          day["haksa"] = temp;
+          monthObject.schedule.push(day);
         });
-      console.log(monthObject);
+      haksa.month.push(monthObject);
     });
-
-
-    /*haksatable = $('.table.border_left.border_top_blue.font09 > tbody');
-    haksatable.each(function(i) {
-        strjson += ' "month' + (i + 1) + '" : {';
-
-        $(this).find('tr > td').each(function(j) {
-            temp = $(this).text().replace(/(\s*~\s*)/gi, ' ~ ');
-            temp = temp.replace(/(^\s*)|(\s*$)/gi, '');
-            strjson += '"haksa' + (j + 1) + '" : "' + temp + '", ';
-        })
-        strjson += '"bla" : ""}, '
-    });
-    strjson += '"blank" : ""}';
-    console.log(strjson);*/
-
-    console.log(JSON.stringify(haksa));
     return haksa;
 }
