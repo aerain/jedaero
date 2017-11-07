6e1a633d8a4e5b85dcfc85ec2a54234174a7c17a
Won beom <cwb2310@naver.com>
2017-11-07 20:19:58 +0900

    20171107 baem

--- app.js
+++ app.js
@@ -41,6 +41,7 @@ app.get('/libdetail', libdetail);
 app.get('/food', food);
 app.get('/schoolfood', baekdu);
 app.get('/dormitory', dormitory);
+app.get('/haksa', haksa);
 app.get('/version', function(req, res) {
     res.render('version', { "title": "제대로 버전" });
 });
