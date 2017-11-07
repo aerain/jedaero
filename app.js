ac3717fbeeb04f7f0bf0dced6d6f751a34d2a6cb
kimtype <kimtybe@naver.com>
2017-11-07 19:35:47 +0900

    20171107

--- app.js
+++ app.js
@@ -32,8 +32,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
-// app.get('/schoolfood', baekdu);
-// app.get('/haksa', haksa);
 app.get('/libsearch', libserch);
 app.get('/timetable', timetable);
 app.get('/libseat', libseat);
@@ -59,7 +57,12 @@ app.get('/food_sort', function(req,res) {
 app.get('/shuttlebus', function(req,res) {
     res.render('info/shuttlebus', {"title": "순환버스"});
 });
-
+app.get('/haksa', function(req,res) {
+    res.render('info/haksa', {"title": "학사일정"});
+});
+app.get('/schoolfood', function(req,res) {
+    res.render('info/schoolfood', {"title": "학식"});
+});
 app.use(function(req, res, next) {
     var err = new Error('Not Found');
     err.status = 404;
