07aab1ef8efee645adba11ad33753e1b0c9725a2
wkdlfhtm1 <wkdlfhtm1@naver.com>
2017-11-07 20:22:42 +0900

    죽여줘

--- routes/libserch.js
+++ routes/libserch.js
@@ -4,29 +4,35 @@ var request = require('request');
 var cheerio = require('cheerio');
 var iconv = require('iconv-lite');
 
+
+
 router.get('/libsearch', function(req, res, next) {
-    url = "http://lib.jejunu.ac.kr/pyxis-api/1/collections/1/search?";
-    viewurl= "http://lib.jejunu.ac.kr/pyxis-api/1/biblios/732900/items";
-    if(req.query.keyword !== undefined) {
-        url = "http://lib.jejunu.ac.kr/pyxis-api/1/collections/1/search?all=k|a|" + encodeURI(req.query.keyword) + "&facet=false&max=65535";
+
+    if (req.query.keyword !== null) {
+        console.log(req.query.keyword);
+        url = "http://lib.jejunu.ac.kr/pyxis-api/1/collections/1/search?all=1|k|a|" + encodeURI(req.query.keyword) + "&facet=false&max=65535";
+        console.log(url);
+        request(url, function (err, ress, html) {
+            if (!err) {
+                dbjson = JSON.parse(html);
+                dbjson.title = "ㅗ";
+                dbjson.keyword = req.query.keyword;
+                console.log(dbjson.success);
+                res.render('info/libserch', dbjson);
+            }
+        });
+    } else {
+        url = "http://lib.jejunu.ac.kr/pyxis-api/1/collections/1/search?all=1|k|a|"
+        request(url, function (err, ress, html) {
+            if (!err) {
+                dbjson = JSON.parse(html);
+                dbjson.title = "ㅗ";
+                console.log(dbjson.success);
+                res.render('info/libserch', dbjson);
+            }
+        });
     }
-    request(url, function(err, ress, html) {
-        if (!err) {
-            dbjson = JSON.parse(html);
-            dbjson.title = "영어공부해";
-
-
-            request(viewurl, function (err, ress, html) {
-                if (!err) {
-                    DBjson = JSON.parse(html);
-                  //  console.log(req);
-                    res.render('info/libserch',dbjson);
-
-                }
-            })
-        }
-    });
-
 });
+
 
 module.exports = router;
