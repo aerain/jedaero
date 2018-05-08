b7be154513c1de75e67600dc776f6adb2c965247
aerain <j40f893@gmail.com>
2018-05-08 23:11:24 +0900

    학식 크롤링 수정

--- public/javascripts/baekdu.js
+++ public/javascripts/baekdu.js
@@ -2,10 +2,14 @@ module.exports = function ($) {
   strjson = '{ "title" : "백두관 식당", ';
   countday = 1;
   countmenu = 1;
-  $('.table.border_left.border_top_blue > tbody > tr > td > p').each(function() {
-    strjson += '"baekdu' + countday + "_" + countmenu + '" : "' + $(this).text() + '", ';
+  $('.table.border_left.border_top_blue > tbody > tr > td').each(function() {
+    TempText = $(this).text()
+    TempText = TempText.substring(1);
+    TempText = TempText.trim();
+
+    strjson += '"baekdu' + countday + "_" + countmenu + '" : "' + TempText + '", ';
     countmenu++;
-    if (countmenu % 9 === 0){
+    if (countmenu % 14 === 0){
       countmenu = 1;
       countday++;
     }
